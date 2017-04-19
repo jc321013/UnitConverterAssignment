44b31b2705b99c6c39e1eaa3d2450b904e58c324
jc321013 <jared.marcolongo@my.jcu.edu.au>
2017-04-19 23:58:12 +1000

    Initial commit

---
+++ app/src/main/java/com/example/jc321013/unitconverterassignment/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.jc321013.unitconverterassignment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1, e2, e3, e4;
+    Button b1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.editText);
+        e2 = (EditText) findViewById(R.id.editText2);
+        e3 = (EditText) findViewById(R.id.editText3);
+        e4 = (EditText) findViewById(R.id.editText4);
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            //            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+
+                double cm = Double.parseDouble(e1.getText().toString());
+                double m = cm / 100;
+                double inch = cm / 2.54;
+                double foot = inch / 12;
+
+                e2.setText("" + m);
+                e3.setText("" + inch);
+                e4.setText("" + foot);
+
+
+            }
+        });
+
+    }
+}
+
+
+    //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
+//
